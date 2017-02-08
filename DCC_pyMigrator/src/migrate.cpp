a660288b9de30fe3922bdf2a1b97a3a8579ca6b2
Henry Borchers <henryborchers@yahoo.com>
2017-02-08 17:25:23 -0600

    refactored to put all wrapper functions here

--- DCC_pyMigrator/src/migrate.cpp
+++ DCC_pyMigrator/src/migrate.cpp
@@ -6,7 +6,10 @@
 */
 
 #include <Python.h>
+#include "convert.h"
+#include "metadata.h"
 
+// *******************************************************************************
 PyObject *migrateHelloWorld(PyObject *self, PyObject *args, PyObject *kwargs) {
     char *s = NULL;
     char result[64];
@@ -27,18 +30,62 @@ PyObject *migrateHelloWorld(PyObject *self, PyObject *args, PyObject *kwargs) {
 PyObject *version(PyObject *self) {
     return Py_BuildValue("s", "Version 0.1");
 }
+// *******************************************************************************
+// Conversion Methods
+static PyObject *pyConvert(PyObject *self, PyObject *args, PyObject *kwargs) {
+    char *input = NULL;
+    char *output = NULL;
+    static PyObject *my_error;
+
+    static char *kwlist[] = {
+            (char *) "input",
+            (char *) "output",
+            NULL
+    };
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "ss", kwlist, &input, &output)) {
+        return NULL;
+    }
+    if (convert(input, output) != 0) {
+        puts("Failing");
+
+        my_error = PyErr_NewException("pymagick.convert", NULL, NULL);
+        PyErr_SetString(my_error, get_error_str());
+        return NULL;
+    }
+
+    Py_RETURN_NONE;
+}
+
+// *******************************************************************************
+// Metadata Methods
+static PyObject *py_read_data(PyObject *self, PyObject *args, PyObject *kwargs) {
+    char *source = NULL;
+    static char *kwlist[] = {"name", NULL};
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s", kwlist, &source)) {
+        return NULL;
+    }
+    read_data(source);
+
+    Py_RETURN_NONE;
+}
 
 
+// *******************************************************************************
+// Registration functions
 static PyMethodDef migrateMethods[] = {
-        {"hello",   (PyCFunction) migrateHelloWorld, METH_VARARGS | METH_KEYWORDS, "Hello World."},
-        {"version", (PyCFunction) version,           METH_NOARGS,                  "Returns the version."},
-        {NULL,      NULL,                            0,                            NULL}
+        {"hello",         (PyCFunction) migrateHelloWorld, METH_VARARGS | METH_KEYWORDS, "Hello World."},
+        {"version",       (PyCFunction) version,           METH_NOARGS, "Returns the version."},
+        {"read_metadata", (PyCFunction) py_read_data,      METH_VARARGS | METH_KEYWORDS, "reads metadata"},
+        {"convert", (PyCFunction) pyConvert,           METH_VARARGS | METH_KEYWORDS, "Execute conversion."},
+        {"version", (PyCFunction) version,             METH_NOARGS, "Returns the version."},
+        {NULL, NULL, 0,                                                                  NULL}
 };
 
 static struct PyModuleDef migrateModule = {
         PyModuleDef_HEAD_INIT,
         "migrate", // name of module
-        NULL, // module documentation, may be NULL 
+        NULL, // module documentation, may be NULL
         -1,/* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
         migrateMethods // Methods
 };
