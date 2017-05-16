dd07440db5d39daba81ff66e835984b6465268d2
Henry Borchers <henryborchers@yahoo.com>
2017-05-16 12:30:31 -0500

    init commit

---
+++ src/Exiv2Wrapper/Exiv2Wrapper.h
@@ -0,0 +1,10 @@
+//
+// Created by Borchers, Henry Samuel on 3/10/17.
+//
+
+#ifndef DCC_MIGRATOR_EXIV2WRAPPER_H
+#define DCC_MIGRATOR_EXIV2WRAPPER_H
+
+#include "ImageMetadata.h"
+#include "reader.h"
+#endif //DCC_MIGRATOR_EXIV2WRAPPER_H
