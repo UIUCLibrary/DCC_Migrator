086facf3e9b55de48aa6dbe6579ae168f7c62715
Henry Borchers <henryborchers@yahoo.com>
2017-05-16 12:33:35 -0500

    building extension now works....kinda

--- setup.py
+++ setup.py
@@ -1,29 +1,37 @@
 from distutils.util import get_platform
 
 from setuptools import setup
+from setuptools.command.build_ext import build_ext as _build_ext
 from distutils.command.build import build as _build
-from distutils.command.build_ext import build_ext as _build_ext
+# from distutils.command.build_ext import build_ext as _build_ext
 from subprocess import check_call
 import os
-
+import sysconfig
+import sys
 
 
 class build_ext(_build_ext):
     def run(self):
-        root = os.path.dirname(os.path.abspath(__file__))
-        build_path = os.path.join(root, self.build_lib)
-
-        if not os.path.exists(build_path):
-            os.makedirs(build_path)
-
-        library_path = os.path.join(root if self.inplace else build_path, "DCC_pyMigrator/ext")
-        check_call(["cmake", "../..", "-DLIBRARY_OUTPUT_PATH={}".format(library_path)], cwd=build_path)
-        check_call(["cmake", "--build", ".", "--config", "Release", "--target", "PythonExtensions"], cwd=build_path)
         _build_ext.run(self)
+        root = os.path.dirname(os.path.abspath(__file__))
+
+        # configure
+        # print("Configuring cmake")
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        configure_command = ["cmake", root,
+                             "-DPYTHON_INCLUDE_DIR={}".format(sysconfig.get_config_vars()["INCLUDEPY"])]
+        check_call(configure_command, cwd=self.build_temp)
+        # build
+
+        build_command = ["cmake", "--build", ".", "--config", "Release", "--target", "DCC_pyMigrator"]
+        check_call(build_command, cwd=self.build_temp)
 
 
 class build(_build):
     sub_commands = [("build_ext", None)]
+
 
 cmdclass = {"build": build,
             "build_ext": build_ext
@@ -39,6 +47,7 @@ try:
             tag = (tag[0], tag[1], get_platform())
             return tag
 
+
     cmdclass["bdist_wheel"] = bdist_wheel
 
 except ImportError:
@@ -53,6 +62,10 @@ setup(
               ],
     cmdclass=cmdclass,
     package_data={"DCC_pyMigrator.ext": ["*.so", "*.pyd"]},
+    tests_require={
+        "pytest"
+    },
+    test_suite="DCC_pyMigrator.tests",
     url="",
     license="",
     author="hborcher",
